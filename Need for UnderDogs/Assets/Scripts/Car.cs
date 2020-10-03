445343c464e7c34ab9825498dfac25e3d3bcb83f
SubhamPaul21 <subhampaul12@gmail.com>
2020-10-03 21:40:57 +0530

    Add tracks and player scripts

---
+++ Need for UnderDogs/Assets/Scripts/Car.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Car : MonoBehaviour
+{
+    [Header("Car properties")]
+    //state variables
+    [SerializeField] protected float brakeFactor = 15f;
+    [SerializeField] protected float acceleration = 1f;
+    [SerializeField] protected float maxSpeed = 10f;
+
+    // member variables
+    protected Rigidbody _rb;
+    protected float _currentSpeed = 0f;
+
+    protected virtual void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        FreezeConstraints();
+    }
+
+    private void FreezeConstraints()
+    {
+        _rb.constraints = RigidbodyConstraints.FreezePositionY
+                        | RigidbodyConstraints.FreezeRotationX
+                        | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    protected abstract void ProcessInput();
+}
---
+++ Need for UnderDogs/Assets/Scripts/PlayerCar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerCar : Car
+{
+    [SerializeField] Transform endPos;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        ProcessInput();
+        agent.autoTraverseOffMeshLink = false;
+        transform.LookAt(endPos);
+        agent.SetDestination(endPos.position);
+    }
+
+    protected override void ProcessInput()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _currentSpeed += acceleration * Time.deltaTime;
+            _rb.velocity += Vector3.forward * _currentSpeed;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            _rb.AddForce(-brakeFactor * _rb.velocity);
+        }
+
+        if (_currentSpeed > maxSpeed)
+        {
+            _currentSpeed = maxSpeed;
+        }
+    }
+}
